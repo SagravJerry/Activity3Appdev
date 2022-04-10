1583a896c83786b282070f1c548054ca91c28173
SagravJerry <SagravJerry>
2022-04-11 00:49:40 +0800

    src/app/tab3/tab3.page.ts modified

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ActionSheetController, AlertController } from '@ionic/angular';
 @Component({
@@ -8,53 +9,51 @@ import { ActionSheetController, AlertController } from '@ionic/angular';
 })
 export class Tab3Page implements OnInit {
   public tab3: string;
+  public formData: FormGroup;
   public items = [
-    {
-      id: 0,
-      name: 'Jerry Vargas',
-      number: '09218217865'
-    },
-    {
-      id: 2,
-      name: 'Jelian Mae',
-      number: '09161651678'
-    },
-     {
-      id: 4,
-      name: 'Jestoni Alamang',
-      number: '09218217865'
-    },
-    {
-      id: 6,
-      name: 'Nica Obrero',
-      number: '09161651678'
-    }
-  ]
-
+    
+   
+  ];
   constructor(private activatedRoute: ActivatedRoute, 
               private alertCtrl:AlertController) {}
 
+
   ngOnInit() {
     this.tab3 = this.activatedRoute.snapshot.paramMap.get('id');
-  }
-  
-  async alertThis(index: number){
-    await this.alertCtrl.create({
-      header: "Alert",
-      message: " Are you sure?",
-      buttons: [{
-        text: 'yes',
-        role: 'destructive',
-        handler: () => {   
-                this.items.splice(index, 1);  
-          console.log ('delete Clicked');
-        }
-      },
-      {
-        text: 'cancel'
-      }
-      ]
+    this.formData= new FormGroup({
+      name: new FormControl(),
+      lastName: new FormControl(),
+      Phone: new FormControl()
+    }); 
+    
 
-    }).then (res=> res.present());
   }
-}
+  onSubmit() {
+    this.items.push(this.formData.value);
+    //  console.log(this.formData.value);
+    this.formData.reset();
+    
+      };
+  
+  
+    async alertThis(index: number): Promise<void>{
+      await this.alertCtrl.create({
+        header: "Alert",
+        message: " Are you sure?",
+        buttons: [{
+          text: 'yes',
+          role: 'destructive',
+          handler: () => {   
+                  this.items.splice(index, 1,);  
+            console.log ('delete Clicked');
+          }
+        },
+        {
+          text: 'cancel'
+        }
+        ]
+  
+      }).then (res=> res.present());
+    }
+
+}  
